2a3d91b43e35e4d937742965390793ec32260926
grishunea80 <grishunea80@mail.ru>
2020-08-01 15:02:59 -0400

    10 prints and new class

---
+++ src/Boat.java
@@ -0,0 +1,5 @@
+public class Boat{
+	public static void main(String[] args){
+	
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public static Main {
 	public static void main(String[] args){
+		for(int i = 0; i < 10; i ++);
 		System.out.println("Hello, World");
+
+		
 	} 
 }
